e23c3c045a95188c44e6bebd50dd3a3b0c094f9b
ItamarTati <itamar.tati1994@gmail.com>
2023-10-28 00:15:35 +0100

    got hero to move

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 let canvas = document.createElement("canvas");
 let context = canvas.getContext("2d");
 
-canvas.width = 480;
-canvas.height = 320;
+canvas.width = 800;
+canvas.height = 700;
 
 document.body.appendChild(canvas);
 
@@ -31,35 +31,60 @@ let hero = {
 };
 
 
-let keysDown = {};
+let keyPresses = {};
 
-addEventListener(
-    "keydown",
-    function (e) {
-        keysDown[e.key] = true;
-    },
-    false
-);
+// Use the 'keydown' event to detect key presses and store them in keyPresses
+document.addEventListener('keydown', keyDownListener, false);
+function keyDownListener(event) {
+    keyPresses[event.key] = true;
+}
 
-addEventListener(
-    "keyup",
-    function (e) {
-        keysDown[e.key] = true;
-    },
-    false
-)
+// Use the 'keyup' event to detect key releases
+document.addEventListener('keyup', keyUpListener, false);
+function keyUpListener(event) {
+    keyPresses[event.key] = false;
+}
 
-function loadImage() {
-    if(backgroundReady) {
-        context.drawImage(backgroundImg, 0, 0);
+function moveChar(deltaX, deltaY, direction) {
+    if (hero.x + deltaX > 0) {
+        hero.x += deltaX + hero.speed;
     }
-    if(heroReady) {
-        context.drawImage(heroImg, 0, 0);
+    if (hero.y + deltaY > 0) {
+        hero.y += deltaY + hero.speed;
+    }
+    currentDirection = direction;
+}
+function moveHero() {
+    if (keyPresses['ArrowUp'] || keyPresses['w']) {
+        // Move the hero up
+        hero.y -= hero.speed;
+    }
+    if (keyPresses['ArrowDown'] || keyPresses['s']) {
+        // Move the hero down
+        hero.y += hero.speed;
+    }
+    if (keyPresses['ArrowLeft'] || keyPresses['a']) {
+        // Move the hero left
+        hero.x -= hero.speed;
+    }
+    if (keyPresses['ArrowRight'] || keyPresses['d']) {
+        // Move the hero right
+        hero.x += hero.speed;
     }
 }
 
+
+function loadImage() {
+    if (backgroundReady) {
+        context.drawImage(backgroundImg, 0, 0);
+    }
+    if (heroReady) {
+        context.drawImage(heroImg, hero.x, hero.y);
+    }
+}
 function gameLoop() {
     loadImage();
+    moveHero();
     requestAnimationFrame(gameLoop);
 }
 
